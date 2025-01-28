b3fb4c742f1da2b1b30ca90c11bd0c4dfbcf8308
zeikomi552 <premium.tsubu@gmail.com>
2025-01-29 04:48:46 +0900

    インターフェースの追加

---
+++ Ollapi/Interface/IOllapiMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Ollapi.Interface
+{
+    public interface IOllapiMessage
+    {
+        public string Role { get; set; }
+
+        public string Content { get; set; }
+
+        public string? Images { get; set; }
+    }
+}
